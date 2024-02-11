002052401ddc7ae4ea18987ebf482c09779003ed
oxe <dococelot@gmail.com>
2024-02-10 21:32:59 -0800

    subtract

--- fixedy/fixedy/fixed_study1.c
+++ fixedy/fixedy/fixed_study1.c
@@ -7,6 +7,7 @@ typedef uint8_t u1;
 
 const u1      u1_max =  255u;
 const s1      s1_max =  127;
+const u1      u1_min =    0u;
 const s1      s1_min = -128;
 const uint8_t s1_shf =    8u;
 const uint8_t s1_msk =  u1_max;
@@ -16,26 +17,13 @@ typedef struct {
     u1 lo;
 } s2;
 
+const s2 s2_max = { .hi =  127, .lo = 255u };
+const s2 s2_min = { .hi = -128, .lo = 255u };
+
 bool isNeg(const s2 a) {
     return a.hi < 0;
 }
 
-s1 addSat(const s1 a, const s1 b) {
-    bool negA   = a < 0;
-    bool negB   = b < 0;
-    bool hetero = negA != negB;
-
-    if (hetero) {
-        // no overflow with a negative and a positive.
-        return a + b;
-    }
-
-
-    return s1_max;
-}
-
-s2 s2_max = { .hi =  127, .lo = 255 };
-s2 s2_min = { .hi = -128, .lo = 255 };
 
 // -1, 0, 1
 int s2_cmp(const s2 a, const s2 b) {
@@ -51,7 +39,6 @@ int s2_cmp(const s2 a, const s2 b) {
     }
 
     int ret = a.lo < b.lo ? -1 : 1;
-
     return ret;
 }
 
@@ -67,12 +54,17 @@ int16_t int16_from_s2(const s2 a) {
 
 s2 s2_add(const s2 a, const s2 b) {
     s2 ret;
-    bool aNeg     = isNeg(a);
-    bool bNeg     = isNeg(b);
-    bool sameSign = aNeg == bNeg;
     s1 carry = (s1)(a.lo > u1_max - b.lo);
     ret.lo = a.lo + b.lo;
     ret.hi = a.hi + b.hi + carry;
+    return ret;
+}
+
+s2 s2_sub(const s2 a, const s2 b) {
+    s2 ret;
+    s1 borrow = (s1)(a.lo < u1_min + b.lo);
+    ret.lo = a.lo - b.lo;
+    ret.hi = a.hi - b.hi - borrow;
     return ret;
 }
 
@@ -90,7 +82,6 @@ void test_conversion(void) {
             printf("uh oh: %d -> %d\n", p, b);
             return;
         }
-
         p += 1;
     } while (p < INT16_MAX);
 
@@ -120,9 +111,8 @@ void test_add(void) {
         int16_t p = fun_values[n];
         s2 a = s2_from_int16(p);
         for (int m = 0; m < ARRAYSIZE(fun_values); ++m) {
-            int16_t q = fun_values[m];
-            s2 b = s2_from_int16(q);
-
+            int16_t q     = fun_values[m];
+            s2      b     = s2_from_int16(q);
             s2      c     = s2_add(a, b);
             int16_t op_ab = int16_from_s2(c);
             int16_t op_pq = p + q;
@@ -140,6 +130,32 @@ void test_add(void) {
     printf("passed.\n");
 }
 
+void test_sub(void) {
+    printf("%-*s()..  ", _format_func_width, __func__);
+
+    for (int n = 0; n < ARRAYSIZE(fun_values); ++n) {
+        int16_t p = fun_values[n];
+        s2 a = s2_from_int16(p);
+        for (int m = 0; m < ARRAYSIZE(fun_values); ++m) {
+            int16_t q     = fun_values[m];
+            s2      b     = s2_from_int16(q);
+            s2      c     = s2_sub(a, b);
+            int16_t op_ab = int16_from_s2(c);
+            int16_t op_pq = p - q;
+
+            if (op_ab != op_pq) {
+                // do it again, for breakpointing:
+                s2      c     = s2_sub(a, b);
+                int16_t op_ab = int16_from_s2(c);
+                printf("uh oh: s2_sub(%d, %d) -> %d, expected %d\n", p, q, op_ab, op_pq);
+                return;
+            }
+        }
+    }
+
+    printf("passed.\n");
+}
+
 
 void test_compare(void) {
     printf("%-*s()..  ", _format_func_width, __func__);
@@ -149,11 +165,10 @@ void test_compare(void) {
         s2 a = s2_from_int16(p);
         for (int m = 0; m < ARRAYSIZE(fun_values); ++m) {
             int16_t q = fun_values[m];
-            s2 b = s2_from_int16(q);
-
+            s2 b      = s2_from_int16(q);
             int op_ab = s2_cmp(a, b);
             int op_pq = p == q ? 0 : (p < q ? -1 : 1);
-
+            
             if (op_ab != op_pq) {
                 // do it again, for breakpointing:
                 int op_ab = s2_cmp(a, b);
@@ -161,7 +176,6 @@ void test_compare(void) {
                 return;
             }
         }
-
     }
 
     printf("passed.\n");
@@ -171,11 +185,10 @@ void test(void) {
     test_conversion();
     test_compare();
     test_add();
+    test_sub();
 }
 
-int main(int argc, const char * argv[]) {
+int main() {
     test();
-    printf("hey\n");
-    return 0;
 }
 
